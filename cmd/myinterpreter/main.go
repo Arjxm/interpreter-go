0a7cbbe6820ae1b60e4d058175b1995f08adc4ba
arjun <arjun232001@gmail.com>
2024-08-25 15:50:27 +0530

    Scanning: Parentheses

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -31,8 +31,17 @@ func main() {
 	}
 
 	if len(fileContents) > 0 {
-		panic("Scanner not implemented")
+		for _, c := range fileContents {
+			if c == '(' {
+				fmt.Println("LEFT_PAREN ( null")
+			} else {
+				fmt.Println("RIGHT_PAREN ) null")
+			}
+		}
+		fmt.Println("EOF  null")
+
 	} else {
+
 		fmt.Println("EOF  null") // Placeholder, remove this line when implementing the scanner
 	}
 }
